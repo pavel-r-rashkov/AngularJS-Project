7b75567444bd86739dab374c2abbe1558523dae3
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2015-05-10 21:47:52 +0300

    add users service methods

--- app/components/users/controllers/usersController.js
+++ app/components/users/controllers/usersController.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('usersModule')
-    .controller('usersController', [function() {
+    .controller('usersController', ['usersService', function(usersService) {
+        //usersService.login('sasho', 'password').then(
+        //    function(data) {
+        //
+        //    },
+        //    function(error) {
+        //
+        //    });
+//username, password, confirmPassword, name, email
+        //usersService.getUserData('sasho').then(
+        //    function(data) {
+        //        console.log(data['data']);
+        //    },
+        //    function(error) {
+        //        console.log(error['data']);
+        //    });
 
+        //usersService.register('testUserName', 'password', 'password', 'myName', 'mail@mail.bg').then(
+        //    function(data) {
+        //
+        //    },
+        //    function(error) {
+        //
+        //    });
     }]);
--- app/components/users/services/users-service.js
+++ app/components/users/services/users-service.js
@@ -1,1 +1,74 @@
+angular.module('usersModule')
+    .factory('usersService', ['$http', function($http) {
+        function getConfig() {
+            var config = {};
+            if(localStorage['sessionToken']) {
+                config.headers = {};
+                config.headers.Authorization = 'Bearer ' + localStorage['sessionToken'];
+            }
 
+            return config;
+        };
+
+        function setUserData(username, sessionToken) {
+            localStorage['username'] = username;
+            localStorage['sessionToken'] = sessionToken;
+        }
+
+        function deleteUserData() {
+            delete localStorage['username'];
+            delete localStorage['sessionToken'];
+        }
+
+        var baseUrl = 'http://softuni-social-network.azurewebsites.net/api/users';
+
+        var usersRepo = {
+            login: function(username, password) {
+                var params = {
+                    username: username,
+                    password: password
+                };
+
+                return $http.post(baseUrl + '/login', params).then(function(data) {
+                    setUserData(data['data']['userName'], data['data']['access_token']);
+                });
+            },
+            logout: function() {
+                return $http.post(baseUrl + '/logout', null, getConfig()).then(function() {
+                    deleteUserData();
+                });
+            },
+            register: function(username, password, confirmPassword, name, email) {
+                var params = {
+                    username: username,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                    name: name,
+                    email: email
+                };
+                return $http.post(baseUrl + '/register', params).then(function(data) {
+                    setUserData(data['data']['userName'], data['data']['access_token']);
+                });
+            },
+            getUserData: function(username) {
+                return $http.get(baseUrl + '/' + username, getConfig());
+            },
+            getUserPreviewData: function(username) {
+                return $http.get(baseUrl + '/' + username + '/preview', getConfig());
+            },
+            searchUsersByName: function(searchTerm) {
+                return $http.get(baseUrl + '/search?searchTerm=' + searchTerm, getConfig());
+            },
+            getFriendWall: function(username, startPostId, pageSize) {
+                return $http.get(baseUrl + '/' + username + '/wall?StartPostId=' + startPostId + '&PageSize=' + pageSize, getConfig());
+            },
+            getFriendsList: function(username) {
+                return $http.get(baseUrl + '/' + username + '/friends', getConfig());
+            },
+            getFriendsPreview: function(username) {
+                return $http.get(baseUrl + '/' + username + '/friends/preview', getConfig());
+            }
+        };
+
+        return usersRepo;
+    }]);
