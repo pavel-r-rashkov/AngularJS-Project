8cc2cc9cf3509fa1fd312cd77dd94e8d73fde3f7
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2015-05-22 14:33:18 +0300

    fix user preview to show friend button depending on friend status

--- app/components/users/directives/user-preview-directive.js
+++ app/components/users/directives/user-preview-directive.js
@@ -4,11 +4,16 @@ angular.module('usersModule').directive('showUserPreview', function () {
             username: '=username'
         },
         templateUrl: 'components/users/views/user-preview.html',
-        controller: function ($scope, $element, $attrs, usersService, friendsService) {
+        controller: function ($scope, $element, $attrs, usersService, friendsService, credentialsService) {
+            var currentUsername = credentialsService.getCurrentUser().username;
+
             usersService.getUserPreviewData($scope.username)
                 .then(
                 function(data) {
                     $scope.userData = data['data'];
+                    $scope.friendStatus = data['data']['hasPendingRequest'] ? 'pending' : 'notFriend';
+                    $scope.friendStatus = data['data']['isFriend'] ? 'friend' : $scope.friendStatus;
+                    $scope.friendStatus = $scope.username === currentUsername ? '': $scope.friendStatus;
                 },
                 function() {
                     console.log('error getting user preview data');
